e72f0b79177b3a175a30c714fb7622aea503e941
Tyrone <tyronefilho.dev@gmail.com>
2026-02-14 19:04:07 -0300

    Ajeitando Logicas e retirando lenght_error

--- PiramidePascal.cpp
+++ PiramidePascal.cpp
@@ -7,9 +7,14 @@ int main()
     //  Este programa mostra a piramide de pascal ate o numero Expoente_A_B;
     std::vector<int> array{1};
 
-    int Expoente_A_B = 11;
-    std::cout << std::string(19, '*') << "PIRAMIDE DE PASCAL" << std::string(19, '*') << "\n\n";
-    std::cout << std::string(28, ' ') << 1 << "\n";
+    int Expoente_A_B = 15; // faixa de existencia de 1 a 24
+    if (Expoente_A_B<0 || Expoente_A_B>24){
+        std::cout << "Valor Expoente menor que 0 ou maior que 24\n" << "Definindo Valor Base Como 5" << "\n\n";
+        Expoente_A_B = 5;
+    }
+
+    std::cout << std::string(Expoente_A_B * 2 + 1, '*') << "PIRAMIDE DE PASCAL" << std::string(Expoente_A_B * 2 + 1, '*') << "\n\n"; // logica para o DE ficar alinhado com o 1 abaixo
+    std::cout << std::string((Expoente_A_B * 2 + ((int)(Expoente_A_B / 5) + 6)), ' ') << 1 << "\n";                                  // logica para alinhar o 1 com base no tamanho da piramide
 
     for (int i = 0; i < Expoente_A_B; i++)
     {
@@ -26,10 +31,10 @@ int main()
 
         array = arr_troca;
 
-        std::cout << std::string(25 - (array.size() + i), ' ');
-        for (int valor : array)
+        std::cout << std::string((((Expoente_A_B * 2) + 2) - (array.size() + i)), ' '); // logica para nao dar 'std::length_error'
+        for (const int &valor : array)
         {
-            std::cout << std::setw((array.size() - i) * 2) << valor;
+            std::cout << std::setw((array.size() - i) * ((int)(Expoente_A_B / 10) + 2)) << valor;
         }
         std::cout << "\n";
     }
